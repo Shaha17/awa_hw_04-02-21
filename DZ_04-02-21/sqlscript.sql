eec918c7d74cc2cb2966c2c9c67245d6efb73020
ShahaKB <shahzodjabbarov@gmail.com>
2021-02-05 22:20:42 +0500

    add sql script

---
+++ DZ_04-02-21/sqlscript.sql
@@ -0,0 +1,21 @@
+USE [AlifAcademy]
+GO
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Person](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[LastName] [nvarchar](200) NOT NULL,
+	[FirstName] [nvarchar](200) NOT NULL,
+	[MiddleName] [nvarchar](200) NOT NULL,
+	[BirthDate] [date] NOT NULL,
+ CONSTRAINT [PK_Person] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
